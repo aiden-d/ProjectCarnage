49ab6824b7170d1d91627f045f7424b1e5ae9c5d
aidend <aidendawes@gmail.com>
2019-05-13 10:50:40 +0200

    Day1 Work Session1
    Brought in and animated demo character and setup demo scene

---
+++ Assets/Scripts/CharacterController/CharacterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public Animator anim;
+    public float health;
+    public bool isBlocking;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
